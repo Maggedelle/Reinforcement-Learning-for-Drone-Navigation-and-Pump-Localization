2fa1fff145e72518d1b922963438bf4a1b310e0d
Magnus Axelsen <pizza.manden@hotmail.co.uk>
2024-06-01 18:59:48 +0200

    add shielding to bfs

--- stompc/bfs.py
+++ stompc/bfs.py
@@ -1,7 +1,7 @@
 from collections import deque
 from classes import State, DroneSpecs, TrainingParameters
-
-def get_action_from_bfs(state:State):
+from utils import check_if_drone_can_see_pump, shield_action
+def get_action_from_bfs(state:State, drone_specs: DroneSpecs):
 
     rows, cols = len(state.map), len(state.map[0])
     queue = deque([(state.map_drone_index_y, state.map_drone_index_x, -1)])
@@ -31,7 +31,8 @@ def get_action_from_bfs(state:State):
         for i, (dr, dc) in enumerate(directions):
             nr, nc = r + dr, c + dc
 
-            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc]:
+            #add the cell to queue if it has not been visited, and it is safe to take
+            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and not shield_action(i + 10, state, drone_specs):
                 visited[nr][nc] = True
                 parents[nr][nc] = (r, c)
                 queue.append((nr,nc, i))
